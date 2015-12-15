a924972194d5a1760ab1df4c35f96472bd0098d3
Michael Vostrikov <michael.vostrikov@gmail.com>
2015-12-15 17:37:04 +0500

    Deleted unnecessary files

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -28,14 +28,4 @@ class SiteController extends Controller
             ],
         ];
     }
-
-    /**
-     * Displays homepage.
-     *
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
 }
